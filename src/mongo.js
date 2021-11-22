c751b6af3e08849bc6c85ca48f2d78b8782a5a6a
Kooper Lingohr <70509747+KooperL@users.noreply.github.com>
2021-11-23 09:52:55 +1100

    Update mongo.js

--- src/mongo.js
+++ src/mongo.js
@@ -1,61 +1,55 @@
+const fs = require('fs');
 const { MongoClient } = require('mongodb');
 //import { MongoClient } from 'mongodb';
-const fabric = require("fabric").fabric;
+// const fabric = require("fabric").fabric;
 //import { fabric } from "fabric";
-const url = 'mongodb://139.99.238.84:27017'; //27017
-const client = new MongoClient(url);//removed url
-const client2 = new MongoClient(url);//removed url
+const url = 'mongodb://139.99.238.84:27017'; 
+const client = new MongoClient(url);
 
-async function call(coll = 'loc_data') {
-    await client.connect();
-    const db = client.db('vic_properties');
-    const locCollection = db.collection(coll);
-    const docs = await locCollection.find({}).toArray();
-    return docs
+
+
+async function call(coll='loc_data', filter={}) {
+    return new Promise((resolve) => {
+        client.connect().then(() => {
+            const db = client.db('vic_properties');
+            const locCollection = db.collection(coll);
+            const docs = locCollection.find(filter).toArray();
+            docs.then((a)=> {
+                // console.log(a);
+                client.close()
+                resolve(a);
+            })
+        })
+    })
 }
 
 async function main(min=300000, max=1500000) {
-
-    await client2.connect();
-
-    const db2 = client2.db('vic_properties');
-
-    const priceCollection = db2.collection('price_data');
-
-    // console.log(docs);
+    const docs = await call();
+    console.log(docs);
     let len;
     let col;
     let price;
     let mean;
+    let p;
+    let block;
     let objs = new Array(docs.length);
-    docs.map(async function callback(block, index) {
-        let price = await priceCollection.find({'suburb': block.suburb}).toArray();
-        if (price.length) {
-            mean = price[0].pricedata.mean_means;
-    } else {
-        mean = 5;
-    }
-
-/*            len = new Array(block.bounds.length);
-            for(i=0;i<block.bounds.length;i++) {
-        // console.log(block.bounds[i])
-                len[i] = {'x': block.bounds[i][0], 'y': block.bounds[i][1]+40};
-            }
-*/
+    for (i=0;i<docs.length;i++) {
+        block = docs[i];
+        let price = await call('price_data', {'suburb': block.suburb});
+        if (price.length) {mean = price[0].pricedata.mean_means;} else {mean = 5;}
         len = await block.bounds.map(function (arr) {return {'x': arr[0], 'y': arr[1]+40}});
-            await Promise.all(len)
         mean = min*(mean<min) + max*(mean>max) + (mean>min)*(mean<max)*mean;
-            col = ((mean-min)/(max-min)).toString(16);
-            objs[index] = new fabric.Polygon(len, {fill: 'green'});
-            //canvas.add(polygon);
+        col = ((mean-min)/(max-min))*255;
+        // objs[index] = new fabric.Polygon(len, {fill: 'green'});
+        // objs[i] = {bounds: len, colour: col.toString(16)};
+        objs[i] = {bounds: len, colour: col.toString(16)};
+        // console.log(objs);
+        //canvas.add(polygon);
         // docs.length = 0;
-    });
-    await Promise.all(objs);
+    };
     console.log('done');
-    console.log(objs);
+    fs.writeFileSync('./data.json', JSON.stringify(objs, null, 2) , 'utf-8');
     return objs;
 }
 
-
-let sss = main().catch(console.error)
-    .finally(() => client.close());
+main();
